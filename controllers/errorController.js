a3ca69995d5da8ec76f0b5b07272b641e284d9ee
Ibrahim <ibrahimkholil550@gmail.com>
2020-10-04 20:27:25 +0600

    added development or production based error handling mechanism

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,35 @@
+sendErrorDev = (res, err) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    err: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+sendErrorProd = (res, err) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    res.status(err.statusCode).json({
+      status: 500,
+      message: 'Something went wrong(Internal Server Error)',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(res, err);
+  } else if (process.env.NODE_ENV.trim() === 'production') {
+    sendErrorProd(res, err);
+  }
+
   next();
 };
