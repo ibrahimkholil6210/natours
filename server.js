e4557cc77857984193559bf10310ad3603cdc7e1
Ibrahim <ibrahimkholil550@gmail.com>
2020-10-06 21:19:43 +0600

    added uncaughtException and unhandledRejection handler

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 dotenv.config({ path: './config.env' });
+
+process.on('uncaughtException', (err) => {
+  console.log(
+    'UNCAUGHT EXCEPTION OCCURED : Shutting down! application got burnt'
+  );
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const app = require('./app');
 
 const DB = process.env.DB.replace('<PASSWORD>', process.env.DB_PASSWORD);
@@ -14,12 +23,11 @@ mongoose
   })
   .then((con) => {
     console.log('DB connection established!');
-  })
-  .catch((err) => console.log(err));
+  });
 
 const port = process.env.PORT || 3030;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   if (process.env.NODE_ENV.trim() == 'production') {
     console.log(
       'Danger::In Production! application running on port:' + process.env.PORT
@@ -27,4 +35,14 @@ app.listen(port, () => {
   } else {
     console.log('Application runing on port:' + process.env.PORT);
   }
+});
+
+process.on('unhandledRejection', (err) => {
+  console.log(
+    'UNHANDLED REGECTION OCCURED : Shutting down! application got burnt'
+  );
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
