61294e1abbd1d29b2a6dab3adf2432e7c65f3ecf
Ibrahim <ibrahimkholil550@gmail.com>
2020-10-07 20:13:43 +0600

    added user model and authController with signup handler

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
 
 const router = express.Router();
+
+router.route('/signup').post(authController.signup);
 
 router
   .route('/')
   .get(userController.readAllUsers)
   .post(userController.createNewUser);
+
 router
   .route('/:id')
   .get(userController.readSigleUser)
